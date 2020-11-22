812206660fa7d6024d0854e033e0558191768d4e
Scott <74545689+FelixScott@users.noreply.github.com>
2020-11-22 23:24:00 +0000

    V1.5 Coalesce 3 data type programs
    Imported code from binary tree and linked list programs.
    Cleaned Binary tree collision code, much neater now. Before it used an associated linked and unlinked list for every collision now the binary tree node can store a next and previous node as a linked list does.
    
    ------------Next Changes-----------
    Remove functions need to be added to the binary tree and linked list classes.
    
    A main body facilitating comparison and easy access should be added.

--- main.py
+++ main.py
@@ -21,12 +21,33 @@ class Employee:
                 EmployeeUnlinkedList.append(Employee(line[0], line[1], line[2], line[3], line[4], line[5], line[6]))
                 i += 1
 
+    def LinkedListDataInput(self, FileName, EmployeeLinkedList, EmployeeUnlinkedList):
+        import csv
+        with open(FileName, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            i = 0
+            for line in csv_reader:
+                EmployeeLinkedList.Insert(str(line[4]), i)
+                EmployeeUnlinkedList.append(Employee(line[0], line[1], line[2], line[3], line[4], line[5], line[6]))
+                i += 1
+
+    def BinaryTreeDataInput(self, FileName, EmployeeBinaryTree, EmployeeUnlinkedList):
+        import csv
+        with open(FileName, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            i = 0
+            for line in csv_reader:
+                EmployeeBinaryTree.Insert(str(line[4]), i)
+                EmployeeUnlinkedList.append(Employee(line[0], line[1], line[2], line[3], line[4], line[5], line[6]))
+                i += 1
+
 
 class HashTableNode:
     def __init__(self, Key, Value):
         self.Key = Key
         self.Value = Value
         self.NextNode = None
+
 
 class HashTable:
     def __init__(self, Capacity):
@@ -94,6 +115,152 @@ class HashTable:
                 print("Index {} has {} entries".format(i, Counter))
             i += 1
 
+
+class BinaryTreeNode:
+    def __init__(self, Value, Name):
+        self.Value = Value
+        self.Name = Name
+        self.HigherNode = None
+        self.LowerNode = None
+        self.Collided = False
+        self.CollidedNext = None
+        self.CollidedPrevious = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.Head = None
+
+    def NodeSort(self, Target, Subject):
+        # Be aware this may throw a fault for data that cant use min() and will need a custom sort method
+        # 0: same
+        # 1: Subject is higher
+        # 2: Target is higher
+        if Target == Subject:
+            return 0
+        elif min(Target, Subject) == Target:
+            return 1
+        else:
+            return 2
+
+    def Insert(self, Value, Name):
+        InsertNode = BinaryTreeNode(Value, Name)
+        ComparisonNode = None
+        IsPlaced = False
+        if self.Head is None:
+            self.Head = InsertNode
+            IsPlaced = True
+        else:
+            ComparisonNode = self.Head
+        while IsPlaced is False:
+            if self.NodeSort(InsertNode.Name, ComparisonNode.Name) == 0: # Nodes are equal
+                self.TreeCollision(InsertNode, ComparisonNode)
+                IsPlaced = True
+            elif self.NodeSort(InsertNode.Name, ComparisonNode.Name) == 1: # ComparisonNode is Higher
+                if ComparisonNode.LowerNode is None:
+                    ComparisonNode.LowerNode = InsertNode
+                    IsPlaced = True
+                else:
+                    ComparisonNode = ComparisonNode.LowerNode
+            elif self.NodeSort(InsertNode.Name, ComparisonNode.Name) == 2: # InsertNode is Higher
+                if ComparisonNode.HigherNode is None:
+                    ComparisonNode.HigherNode = InsertNode
+                    IsPlaced = True
+                else:
+                    ComparisonNode = ComparisonNode.HigherNode
+
+    def TreeCollision (self, TargetNode, SubjectNode):
+        SubjectNode.Collided = True
+        TargetNode.Collided = True
+        CurrentNode = SubjectNode
+        while CurrentNode.CollidedNext is not None:
+            CurrentNode = CurrentNode.CollidedNext
+        CurrentNode.CollidedNext = TargetNode
+        TargetNode.CollidedPrevious = CurrentNode
+
+
+    def FindEntry(self, Name):
+        ComparisonNode = self.Head
+        while Name != ComparisonNode.Name:
+            if self.NodeSort(Name, ComparisonNode.Name) == 1:
+                ComparisonNode = ComparisonNode.LowerNode
+            elif self.NodeSort(Name, ComparisonNode.Name) == 2:
+                ComparisonNode = ComparisonNode.HigherNode
+        if ComparisonNode.Collided:
+            print("Multiple entries under {} found:".format(Name))
+            i = 0
+            while ComparisonNode.CollidedNext is not None:
+                print("{}: {} ({})".format(i, ComparisonNode.Name, i))
+                i += 1
+            UserChoice = input("Please choose number from above list: ")
+            i = 0
+            while i < UserChoice:
+                ComparisonNode = ComparisonNode.CollidedNext
+            return ComparisonNode.Value
+        else:
+            return ComparisonNode.Value
+
+
+class LinkedListNode:
+    def __init__(self, Value, Name):
+        self.Value = Value
+        self.Name = Name
+        self.NextNode = None
+        self.PreviousNode = None
+
+
+class LinkedList:
+    # This program uses a linked list for any clashes in ID for the binary tree
+    def __init__(self):
+        self.Head = None
+        self.Tail = None
+
+    def Insert(self, Value, Name):
+        Node = LinkedListNode(Value, Name)
+        if self.Head is None:
+            self.Head = Node
+        elif self.Tail is None:
+            self.Tail = Node
+            self.Tail.PreviousNode = self.Head
+            self.Head.NextNode = Node
+            Node.PreviousNode = self.Head
+        else:
+            self.Tail.NextNode = Node
+            Node.PreviousNode = self.Tail
+            self.Tail = Node
+
+    def PrintLinkedList(self):
+        CurrentNode = self.Head
+        while CurrentNode != self.Tail:
+            print (str(CurrentNode.Name) + " -> ")
+            CurrentNode = CurrentNode.NextNode
+        print(str(CurrentNode.Name))
+
+    def FindEntryFromName(self, NodeName):
+        CurrentNode = self.Head
+        while CurrentNode.Name != NodeName and CurrentNode is not self.Tail:
+            CurrentNode = CurrentNode.NextNode
+        if CurrentNode.Name == NodeName:
+            return CurrentNode.Value
+        else:
+            return 0
+
+    def FindEntryFromPosition(self, NodePosition):
+        CurrentNode = self.Head
+        i = 0
+        while i < NodePosition:
+            CurrentNode = CurrentNode.NextNode
+            i += 1
+        return CurrentNode.Value
+
+    def PresentEntries(self):
+        CurrentNode = self.Head
+        i = 1
+        while CurrentNode != self.Tail:
+            print("{}: {}".format(i, CurrentNode.Name))
+            i += 1
+            CurrentNode = CurrentNode.NextNode
+        print("{}: {}".format(i, CurrentNode.Name))
 
 
 ExampleEmployeeUnlinkedList = []
